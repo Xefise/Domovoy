46f0ac2b4eb3a6a8e08ac017005f4071da02645f
MaxSemukhin <62894648+MaxSemukhin@users.noreply.github.com>
2022-07-16 00:02:08 +0300

    front

--- Domovoy/ClientApp/src/pages/TenantLayout.tsx
+++ Domovoy/ClientApp/src/pages/TenantLayout.tsx
@@ -2,6 +2,15 @@
 import {AnimatePresence, motion} from "framer-motion";
 import {Link, useOutlet} from "react-router-dom";
 import {upVariants} from "../animations";
+import '../styles/menu.css';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import mainIcon from '../images/mainIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
+import chatIcon from '../images/chatIcon.svg';
+import profileIcon from '../images/profileIcon.svg';
 
 export interface Props {
 
@@ -15,9 +24,17 @@ function TenantLayout(props: Props) {
             {Outlet && React.cloneElement(Outlet, {key: location.pathname})}
         </AnimatePresence>
         
-        <nav>Тут навбар</nav>
-        <Link to={''}>Main</Link>
-        <Link to={'search'}>Seach</Link>
+        <nav>
+            <Container>
+                <Row>
+                    <Col><Link to={''} className="nav_link"><img src={mainIcon}/></Link></Col>
+                    <Col><Link to={'search'} className="nav_link"><img src={searchIcon}/></Link></Col>
+                    <Col><Link to={'chat'} className="nav_link"><img src={chatIcon}/></Link></Col>
+                    <Col><Link to={'profile'} className="nav_link"><img src={profileIcon}/></Link></Col>
+                </Row>
+            </Container>
+        </nav>
+
     </motion.div>
 }
 
