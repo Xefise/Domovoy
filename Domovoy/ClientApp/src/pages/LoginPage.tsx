70522aec35b3e5f5a97f6dc85b66976fc95ad5a3
MaxSemukhin <62894648+MaxSemukhin@users.noreply.github.com>
2022-07-15 20:52:21 +0300

    Add files via upload

--- Domovoy/ClientApp/src/pages/LoginPage.tsx
+++ Domovoy/ClientApp/src/pages/LoginPage.tsx
@@ -5,6 +5,12 @@ import OAuth from "../components/OAuth";
 import {upVariants} from "../animations";
 import { motion } from "framer-motion";
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import '../styles/LoginPage.css';
+import logoIcon from '../images/logo.png';
+
 export interface Props {
 
 }
@@ -16,18 +22,18 @@ enum State {
 
 function LoginPage(props: Props) {
     const [state, setState] = useState(State.Login);
-    const changeState = () => {
-        if (state === State.Login) {
-            setState(State.Register);
-        } else {
-            setState(State.Login);
-        }
-    };
     
     return <motion.div variants={upVariants} initial={'init'} animate={'show'} exit={'hide'} className={'layout'}>
+
+        <img src={logoIcon} className="logoIcon"/>
+
+        <Container className="selector">
+            <button type={"button"} className={state == State.Login ? 'select_button active' : 'select_button'} onClick={() => setState(State.Login)}>Войти</button>
+            <button type={"button"} className={state == State.Register ? 'select_button active' : 'select_button'} onClick={() => setState(State.Register)}>Зарегистрироваться</button>
+        </Container>
+
         {state == State.Login && <LoginForm/>}
         {state == State.Register && <RegisterForm/>}
-        <button type={"button"} onClick={changeState}>{state == State.Login ? "Register" : "Login"}</button>
         <OAuth/>
     </motion.div>
 }
