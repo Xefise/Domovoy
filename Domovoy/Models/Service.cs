938c1f7f1c2f060e8603fee6e8b19734d57cfcb8
Xefise <xefise@gmail.com>
2022-07-15 18:32:17 +0300

    Add service&informer

---
+++ Domovoy/Models/Service.cs
@@ -0,0 +1,29 @@
+﻿namespace Domovoy.Models;
+
+public class Service
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+    // Ready -> false (closed)
+    public bool IsActive { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime CloseDate { get; set; }
+    public string Check { get; set; }
+    // Оплачено?
+    public bool HasPaid { get; set; }
+
+    public Apartment Apartment { get; set; }
+    public int ApartmentId { get; set; }
+
+    public PermanentService? PermanentService { get; set; }
+}
+
+public class PermanentService
+{
+    public int Id { get; set; }
+    public Service Service { get; set; }
+
+    public bool AutoPay { get; set; }
+    public string Period { get; set; }
+}
