8651959e1c93384fd55c4cd925904aa07752f748
MaxSemukhin <62894648+MaxSemukhin@users.noreply.github.com>
2022-07-16 00:58:24 +0300

    active

--- Domovoy/ClientApp/src/pages/TenantLayout.tsx
+++ Domovoy/ClientApp/src/pages/TenantLayout.tsx
@@ -1,6 +1,6 @@
 ﻿import React from "react";
 import {AnimatePresence, motion} from "framer-motion";
-import {Link, useOutlet} from "react-router-dom";
+import {Link, useOutlet, useLocation} from "react-router-dom";
 import {upVariants} from "../animations";
 import '../styles/menu.css';
 
@@ -8,16 +8,23 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import mainIcon from '../images/mainIcon.svg';
+import mainActiveIcon from '../images/mainActiveIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import searchActiveIcon from '../images/searchActiveIcon.svg';
 import chatIcon from '../images/chatIcon.svg';
+import chatActiveIcon from '../images/chatActiveIcon.svg';
 import profileIcon from '../images/profileIcon.svg';
+import profileActiveIcon from '../images/profileActiveIcon.svg';
 
 export interface Props {
 
 }
 
 function TenantLayout(props: Props) {
+    const location = useLocation()
     const Outlet = useOutlet()
+
+    console.log(location.pathname)
     
     return <motion.div variants={upVariants} initial={'init'} animate={'show'} exit={'hide'}>
         <AnimatePresence exitBeforeEnter>
@@ -27,10 +34,10 @@ function TenantLayout(props: Props) {
         <nav>
             <Container>
                 <Row>
-                    <Col><Link to={''} className="nav_link"><img src={mainIcon}/></Link></Col>
-                    <Col><Link to={'search'} className="nav_link"><img src={searchIcon}/></Link></Col>
-                    <Col><Link to={'chat'} className="nav_link"><img src={chatIcon}/></Link></Col>
-                    <Col><Link to={'profile'} className="nav_link"><img src={profileIcon}/></Link></Col>
+                    <Col><Link to={''} className="nav_link"><img src={location.pathname == '/' ? mainActiveIcon : mainIcon}/></Link></Col>
+                    <Col><Link to={'search'} className="nav_link"><img src={location.pathname == '/search' ? searchActiveIcon : searchIcon}/></Link></Col>
+                    <Col><Link to={'chat'} className="nav_link"><img src={location.pathname == '/chat' ? chatActiveIcon : chatIcon}/></Link></Col>
+                    <Col><Link to={'profile'} className="nav_link"><img src={location.pathname == '/profile' ? profileActiveIcon : profileIcon}/></Link></Col>
                 </Row>
             </Container>
         </nav>
--- Domovoy/ClientApp/src/pages/TenantProfilePage.tsx
+++ Domovoy/ClientApp/src/pages/TenantProfilePage.tsx
@@ -6,7 +6,9 @@ export interface Props {
 }
 
 function TenantProfilePage(props: Props) {
-    return <motion.div variants={upVariants} initial={'init'} animate={'show'} exit={'hide'}>Test</motion.div>
+    return <motion.div variants={upVariants} initial={'init'} animate={'show'} exit={'hide'}>
+    <p>1111</p>
+    </motion.div>
 }
 
 export default TenantProfilePage;
