32f4b082164481895ca38a7d9e93fb764330ae5a
Neisvestney <neisvestney113@gmail.com>
2022-07-16 08:58:41 +0300

    Add selectedAparment and list SmartHome

--- Domovoy/ClientApp/src/pages/TenantIndexPage.tsx
+++ Domovoy/ClientApp/src/pages/TenantIndexPage.tsx
@@ -1,27 +1,55 @@
 import {useAuth} from "../components/AuthProvider";
 import {useEffect, useState} from "react";
-import {Link} from "react-router-dom";
 import {upVariants} from "../animations";
-import { motion } from "framer-motion";
-import {ApartmentViewModel, TenantAppartamentsService} from "../api";
+import {motion} from "framer-motion";
+import {
+    ApartmentViewModel,
+    SmartHomeDevice, SmartHomeDeviceDTO,
+    SmartHomeDevicesService,
+    SmartHomeDeviceType,
+    TenantAppartamentsService
+} from "../api";
 import {apartamentToAddressSting} from "../addressToString";
 
 function TenantIndexPage() {
     const auth = useAuth()
     
-    const [apartamentsLoading, setApartamentsLoading] = useState(true)
-    const [apartaments, setApartaments] = useState<ApartmentViewModel[]>([])
+    const [apartmentsLoading, setApartmentsLoading] = useState(true)
+    const [apartments, setApartments] = useState<ApartmentViewModel[]>([])
+    const [selectedAparment, setSelectedAparment] = useState<ApartmentViewModel>()
+    
+    const [smartHomeDevices, setSmartHomeDevices] = useState<SmartHomeDeviceDTO[]>()
+    const [smartHomeDevicesLoadng, setSmartHomeDevicesLoadng] = useState(false)
     
     useEffect(() => {
-        TenantAppartamentsService.getApiTenantappartaments().then(d => setApartaments(d)).finally(() => setApartamentsLoading(false))
+        TenantAppartamentsService.getApiTenantappartaments().then(d => setApartments(d)).finally(() => setApartmentsLoading(false))
     }, [])
+    
+    useEffect(() => {
+        if (apartments.length > 0 && !selectedAparment) setSelectedAparment(apartments[0])
+    }, [apartments])
+    
+    useEffect(() => {
+        if (selectedAparment) {
+            setSmartHomeDevicesLoadng(true)
+            SmartHomeDevicesService.getApiSmarthomedevicesApartment(selectedAparment.id!).then(d => setSmartHomeDevices(d)).finally(() => setSmartHomeDevicesLoadng(false))
+        }
+    }, [selectedAparment])
     
     return <motion.div variants={upVariants} initial={'init'} animate={'show'} exit={'hide'}>
         Привет, {auth.user?.userName}! <br/>
-        Ваши квартиры:
-        <ul>
-            {apartaments.map(a => <li>
+        Выбранная квартира: <select value={selectedAparment?.id} onChange={e => setSelectedAparment(apartments.find(a => a.id == parseInt(e.target.value)))}>
+            {apartments.map(a => <option value={a.id}>
                 {apartamentToAddressSting(a)}
+            </option>)}
+        </select>
+        <h3>SmartHome</h3>
+        <ul>
+            {smartHomeDevices?.map(d => <li>
+                {d.smartHomeDeviceType == SmartHomeDeviceType.INTERCOM && "Домофон"}
+                {d.actions?.map(a => <button onClick={() => {
+                    SmartHomeDevicesService.postApiSmarthomedevicesUse(d.id!, a.actionId!)
+                }}>{a.name}</button>)}
             </li>)}
         </ul>
     </motion.div>
